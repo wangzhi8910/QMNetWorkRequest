6a06763117625383d1edc5ae197feacf53bab56f
zhi.wang <zhi.wang@2339.com>
2020-04-29 16:03:25 +0800

    1

---
+++ Example/QMNetWorkRequest/Test.h
@@ -0,0 +1,17 @@
+//
+//  Test.h
+//  QMNetWorkRequest_Example
+//
+//  Created by 王志 on 2020/4/29.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/QMNetWorkRequest/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  QMNetWorkRequest_Example
+//
+//  Created by 王志 on 2020/4/29.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
